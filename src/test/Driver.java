42a1481f6d925b373f8495d3dcb49284ab937062
Aakash Chugh <akshchugh.ac@gmail.com>
2015-07-23 18:47:40 +0530

    Update Driver.java

--- src/test/Driver.java
+++ src/test/Driver.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 import java.util.Set;
 
 public class Driver {
-	public static void main(String[] args) {
+	public static void main(String[] arguments) {
 		Scrabble scrabble = new Scrabble();
 		String word = "_a_";
 
