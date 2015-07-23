f6797e89bfb46c8c683ff916106506a8cf4ebddb
ekanshmittal <ontoekansh@yahoo.com>
2015-07-23 17:06:48 +0530

    changes to constants

--- src/test/Scrabble.java
+++ src/test/Scrabble.java
@@ -2,16 +2,10 @@ package test;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class Scrabble {
